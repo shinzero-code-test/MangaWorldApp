35a9dab2c95a768a588d58978d6f2f8691e8a56c
20Youssef10 <youssef2010.mahmoud@gmail.com>
2026-05-19 23:31:40 +0300

    Fix reader screen font weight import

--- app/src/main/kotlin/com/exapps/mangaworld/presentation/reader/ReaderScreen.kt
+++ app/src/main/kotlin/com/exapps/mangaworld/presentation/reader/ReaderScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
