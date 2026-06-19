74b03e5dcc2c3be1b606d631319365c6995840a8
20Youssef10 <youssef2010.mahmoud@gmail.com>
2026-06-19 02:41:56 +0000

    chore: update version to 3.12.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,8 +72,8 @@ android {
         applicationId = "com.exapps.mangaworld"
         minSdk        = 26
         targetSdk     = 35
-        versionCode   = 58
-        versionName   = "3.12.0"
+        versionCode   = 60
+        versionName   = "3.12.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
