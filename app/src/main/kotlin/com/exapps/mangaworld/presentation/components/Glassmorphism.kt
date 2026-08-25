7065daa6a0fdccde058c61d328e3099fd71c72d8
20Youssef10 <youssef2010.mahmoud@gmail.com>
2026-08-25 13:20:24 +0000

    fix: missing layout imports in Glassmorphism drag handle

--- app/src/main/kotlin/com/exapps/mangaworld/presentation/components/Glassmorphism.kt
+++ app/src/main/kotlin/com/exapps/mangaworld/presentation/components/Glassmorphism.kt
@@ -6,6 +6,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
